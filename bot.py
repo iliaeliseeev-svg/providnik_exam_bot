bc342131bd76f7fd7bfd736aac9c91dfd87a22a8
iliaeliseeev-svg <iliaeliseeev@gmail.com>
2026-09-18 23:37:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -61,8 +61,7 @@ def send_question(chat_id):
     if q["type"] == "single":
         buttons = [
             types.InlineKeyboardButton(str(i), callback_data=f"s|{i}")
-            for i in range(1, len(q["a"]) + 1)
-        ],
+            for i in range(1, len(q["a"]) + 1)] ,
         markup.row(*buttons)
         header = f"❓ Вопрос {u['idx']+1}/{len(u['exam'])}\n\n{q['q']}\n\n{options_text}"
     else:
